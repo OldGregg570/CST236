262db35f1c9fc39148683c87d4bf8acda1cca477
Andrew Bragg <andrew.bragg@autodesk.com>
2015-04-08 20:20:31 -0700

    test

---
+++ source/triangles.py
@@ -0,0 +1,46 @@
+"""
+:mod: source.triangles
+
+methods for determining the class of a triangle defined by its side lengths
+"""
+
+def get_triangle_type(a=0, b=0, c=0):
+    """
+    Determine if the given triangle is equilateral, scalene or Isosceles
+
+    :param a: line a
+    :type a: float or int or tuple or list or dict
+
+    :param b: line b
+    :type b: float
+
+    :param c: line c
+    :type c: float
+
+    :return: "equilateral", "isosceles", "scalene" or "invalid"
+    :rtype: str
+    """
+    if isinstance(a, (tuple, list)) and len(a) == 3:
+        a, b, c = a[0], a[1], a[2]
+
+    if isinstance(a, dict) and len(a.keys()) == 3:
+        values = []
+        for value in a.values():
+            values.append(value)
+        a, b, c = values[0], values[1], values[2]
+
+
+    # TODO: push to parent class
+    if not (isinstance(a, (int, float)) and isinstance(b, (int, float)) and isinstance(c, (int, float))):
+        return "invalid"
+
+    if a <= 0 or b <= 0 or c <= 0:
+        return "invalid"
+
+    if a == b and b == c:
+        return "equilateral"
+
+    elif a == b or a == c or b == c:
+        return "isosceles"
+    else:
+        return "scalene"
