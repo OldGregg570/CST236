323745039d3bd528f6aa68eb7c5233016ec229a2
Andrew Bragg <andrew.bragg@autodesk.com>
2015-04-27 12:17:22 -0700

    finished hello world plugin

---
+++ tests/requirement_test.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from mock import patch
+
+
+class Test(TestCase):
+    def test_test(self):
+        """
+        When defending against Orcs I want a way to tell
+        when one has breached the perimeter so I can deploy defenses
+        """
+        self.assertTrue(True)
