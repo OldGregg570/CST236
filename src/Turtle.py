6d52b748eb89025890c52e5c2f37f9e439cdb4cb
Andrew Bragg <andrew.bragg@autodesk.com>
2015-06-02 21:56:07 -0700

    added documentation and better getters

--- src/Turtle.py
+++ src/Turtle.py
@@ -1,9 +1,24 @@
+import math
+
+DEG_TO_RAD = (2 * math.pi / 360.0)
+
 
 class Turtle(object):
-    def __init__(self):
+    """
+    Turtle class. Defines methods to track position and a facing direction. The turtle can be rotated and moved in the
+    current direction.
+    """
+    def __init__(self, position=(0.0, 0.0)):
         self.__angle = 0
-        self.__position = (0, 0)
-        pass
+        self.__position = position
+
+    @property
+    def x(self):
+        return self.__position[0]
+
+    @property
+    def y(self):
+        return self.__position[1]
 
     @property
     def angle(self):
@@ -19,20 +34,23 @@ class Turtle(object):
 
     @position.setter
     def position(self, position):
-        self.position = position
-
-    def rotate_to(self, angle):
-        self.angle = angle
-        return self
-
-    def rotate_by(self, rhs):
-        self.angle += rhs
-        return self
-
-    def translate_to(self, position):
         self.__position = position
+
+    def rotate(self, angle):
+        """
+        Rotate the turtle by the provided angle ammount
+        :param angle: the angle to rotate by (clockwise)
+        :return: self
+        """
+        self.angle += angle
         return self
 
-    def translate_by(self, rhs):
-        self.__position = (self.position[0] + rhs[0], self.position[1] + rhs[1])
+    def move(self, distance):
+        """
+        Move the turtle by the provided distance. Turtle is to move in the direction of self.angle where 0 is East
+        :param distance: the distance to move by
+        :return: self
+        """
+        self.position = (self.__position[0] + (math.cos(self.__angle * DEG_TO_RAD) * distance),
+                         self.__position[1] + (math.sin(self.__angle * DEG_TO_RAD) * distance))
         return self
