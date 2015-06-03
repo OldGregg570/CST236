cd5d8272fc06ed6928039f927e5c54350c0a22cc
Andrew Bragg <andrew.bragg@autodesk.com>
2015-06-02 20:54:27 -0700

    added new move tests

--- tests/turtle_test.py
+++ tests/turtle_test.py
@@ -31,26 +31,40 @@ class TestTurtle(TestCase):
         turtle.angle = 360
         self.assertEqual(0, turtle.angle)
 
-    def test_absolute_rotate(self):
-        """
-        the turtle will provide a method to rotate it to an absolute angle
-        """
-        self.assertEqual(90, Turtle().rotate_to(90).angle)
-
     def test_relative_rotate(self):
         """
         the turtle will provide a method to rotate it by a relative angle
         """
-        self.assertEqual(120, Turtle().rotate_to(30).rotate_by(90).angle)
+        self.assertEqual(120, Turtle().rotate(30).rotate(90).angle)
 
-    def test_absolute_move(self):
-        """
-        the turtle will provide a method to move it to an absolute position
-        """
-        self.assertEqual((5, 5), Turtle().move_to((5, 5)).position)
-
-    def test_relative_translate(self):
+    def test_relative_move(self):
         """
         the turtle will provide a method to move it forward a specified distance
         """
         self.assertEqual((7.0, 7.0), Turtle((0.0, 7.0)).move(7).position)
+
+
+class TestTurtleMove(TestCase):
+    def test_move_directions(self):
+        """
+        the turtle should be able to move in all directions on the cartesian plane
+        """
+        C = 2**0.5 / 2  # sqrt(2)/2
+        directions = [45, 135, 225, 315]
+        positions = [(C, C), (-C, C), (-C, -C), (C, -C)]
+
+        for pos, dir in zip(positions, directions):
+            t = Turtle()
+            t.angle = dir
+            t.move(1)
+            self.assertAlmostEqual(pos[0], t.x, 3)
+            self.assertAlmostEqual(pos[1], t.y, 3)
+
+    def test_square_walk(self):
+        t = Turtle()
+        for n in range(4):
+            t.move(1)
+            t.rotate(90)
+
+        self.assertAlmostEqual(0, t.x, 3)
+        self.assertAlmostEqual(0, t.y, 3)
