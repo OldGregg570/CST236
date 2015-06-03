b4c5ca56b4a81f8fdd834361e37d7383f6f54c0c
Andrew Bragg <andrew.bragg@autodesk.com>
2015-06-02 18:28:49 -0700

    added first round of code

--- src/Turtle.py
+++ src/Turtle.py
@@ -1,4 +1,38 @@
 
 class Turtle(object):
     def __init__(self):
+        self.__angle = 0
+        self.__position = (0, 0)
         pass
+
+    @property
+    def angle(self):
+        return self.__angle
+
+    @angle.setter
+    def angle(self, angle):
+        self.__angle = angle % 360
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, position):
+        self.position = position
+
+    def rotate_to(self, angle):
+        self.angle = angle
+        return self
+
+    def rotate_by(self, rhs):
+        self.angle += rhs
+        return self
+
+    def translate_to(self, position):
+        self.__position = position
+        return self
+
+    def translate_by(self, rhs):
+        self.__position = (self.position[0] + rhs[0], self.position[1] + rhs[1])
+        return self
--- tests/turtle_test.py
+++ tests/turtle_test.py
@@ -3,35 +3,46 @@ from Turtle import Turtle
 
 
 class TestTurtleTrace(TestCase):
-    turtle = Turtle()
+    def test_angles_positive(self):
+        """
+        the turtle will provide methods to get and set an angle. The angle value in the range of [0, 360)
+        """
+        turtle = Turtle()
+        turtle.angle = 20
+        self.assertEqual(20, turtle.angle)
+
+    def test_angles_overflow(self):
+        turtle = Turtle()
+        turtle.angle = 370
+        self.assertEqual(10, turtle.angle)
+
+    def test_angles_negative(self):
+        turtle = Turtle()
+        turtle.angle = -30
+        self.assertEqual(330, turtle.angle)
+
+
     def test_absolute_rotate(self):
         """
         the turtle will provide a method to rotate it to an absolute angle
         """
-        self.turtle.rotate_to(90)
-        self.assertEqual(90, self.turtle.angle)
-        pass
+        self.assertEqual(90, Turtle().rotate_to(90).angle)
 
     def test_relative_rotate(self):
         """
         the turtle will provide a method to rotate it by a relative angle
         """
-        self.turtle.rotate_to(30).rotate_by(90)
-        self.assertEqual(120, self.turtle.angle)
-        pass
+        self.assertEqual(120, Turtle().rotate_to(30).rotate_by(90).angle)
 
     def test_absolute_translate(self):
         """
         the turtle will provide a method to translate it to an absolute position
         """
-        self.turtle.translate_to((5, 5))
-        self.assertEqual((5, 5), self.turtle.position)
-        pass
+        self.assertEqual((5, 5), Turtle().translate_to((5, 5)).position)
 
-    def tesT_relative_translate(self):
+    def test_relative_translate(self):
         """
         the turtle will provide a method to translate it by a relative distance
         """
-        self.turtle.translate_to((5, 5)).translate_by((2, 2))
-        self.assertEqual((7, 7), self.turtle.position)
-        pass
+        self.assertEqual((7, 7), Turtle().translate_to((5, 5)).translate_by((2, 2)).position)
+
