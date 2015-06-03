b736c27f99ef102cbd1c6becc2ca8eb8f32aee3e
Andrew Bragg <andrew.bragg@autodesk.com>
2015-06-02 20:55:54 -0700

    separated turtle tests into their own file

--- tests/tracer_test.py
+++ tests/tracer_test.py
@@ -1,59 +1,5 @@
 from unittest import TestCase
-from Turtle import Turtle
 from Tracer import Tracer
-
-class TestTurtle(TestCase):
-    def test_angles_positive(self):
-        """
-        the turtle will provide methods to get and set an angle. The angle value in the range of [0, 360)
-        """
-        turtle = Turtle()
-        turtle.angle = 20
-        self.assertEqual(20, turtle.angle)
-
-    def test_angles_overflow(self):
-        turtle = Turtle()
-        turtle.angle = 370
-        self.assertEqual(10, turtle.angle)
-
-    def test_angles_negative(self):
-        turtle = Turtle()
-        turtle.angle = -30
-        self.assertEqual(330, turtle.angle)
-
-    def test_angles_zero(self):
-        turtle = Turtle()
-        turtle.angle = 0
-        self.assertEqual(0, turtle.angle)
-
-    def test_angles_threesixty(self):
-        turtle = Turtle()
-        turtle.angle = 360
-        self.assertEqual(0, turtle.angle)
-
-    def test_absolute_rotate(self):
-        """
-        the turtle will provide a method to rotate it to an absolute angle
-        """
-        self.assertEqual(90, Turtle().rotate_to(90).angle)
-
-    def test_relative_rotate(self):
-        """
-        the turtle will provide a method to rotate it by a relative angle
-        """
-        self.assertEqual(120, Turtle().rotate_to(30).rotate_by(90).angle)
-
-    def test_absolute_translate(self):
-        """
-        the turtle will provide a method to translate it to an absolute position
-        """
-        self.assertEqual((5, 5), Turtle().translate_to((5, 5)).position)
-
-    def test_relative_translate(self):
-        """
-        the turtle will provide a method to translate it by a relative distance
-        """
-        self.assertEqual((7, 7), Turtle().translate_to((5, 5)).translate_by((2, 2)).position)
 
 
 class TestTracer(TestCase):
@@ -66,7 +12,6 @@ class TestTracer(TestCase):
         self.assertEqual((0.0, 0.0), tracer.position)
         self.assertEqual([(0.0, 0.0)], tracer.path)
         pass
-
 
     def test_get_turtle_position(self):
         """
