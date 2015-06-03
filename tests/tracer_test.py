ca3ccd331707f88bb7d6d4ba67f701df9b8b9980
Andrew Bragg <andrew.bragg@autodesk.com>
2015-06-02 21:55:42 -0700

    rewrote some tests and removed some requirements

--- tests/tracer_test.py
+++ tests/tracer_test.py
@@ -3,6 +3,12 @@ from Tracer import Tracer
 
 
 class TestTracer(TestCase):
+
+    def assertListOfPointsAlmostEqual(self, left, right):
+        for p1, p2 in zip(left, right):
+            self.assertAlmostEqual(p1[0], p2[0], 3)
+            self.assertAlmostEqual(p1[1], p2[1], 3)
+
     def test_get_turtle_position(self):
         """
         unless otherwise specified, the tracer's initial position shall be (0.0, 0.0). This initial position is saved
@@ -11,7 +17,6 @@ class TestTracer(TestCase):
         tracer = Tracer()
         self.assertEqual((0.0, 0.0), tracer.position)
         self.assertEqual([(0.0, 0.0)], tracer.path)
-        pass
 
     def test_get_turtle_position(self):
         """
@@ -19,8 +24,7 @@ class TestTracer(TestCase):
         """
         tracer = Tracer()
         tracer.trace(1, 90)
-        self.assertEqual((0.0, 1.0), tracer.position)
-        pass
+        self.assertEqual((1.0, 0.0), tracer.position)
 
     def test_trace_turtle(self):
         """
@@ -28,51 +32,21 @@ class TestTracer(TestCase):
         """
         tracer = Tracer()
         tracer.trace(1, 90)
-        self.assertEqual([(0.0, 0.0), (0.0, 1.0)], tracer.path)
-        pass
+        self.assertEqual([(0.0, 0.0), (1.0, 0.0)], tracer.path)
 
     def test_trace_nosave(self):
         """
         the tracer will provide a method to move and turn the turtle without pushing the resulting position to a list
         """
         tracer = Tracer()
-        tracer.tracer(1, 90, save=False)
+        tracer.trace(1, 90, save=False)
         self.assertEqual([(0.0, 0.0)], tracer.path)
-        pass
-
-    def test_tracer_reset(self):
-        """
-        the tracer will provide a method to reinitialize at a specified position
-        """
-        tracer = Tracer()
-        tracer.trace(1, 90)
-        tracer.trace(1, 90)
-        tracer.reset((1.0, 1.0))
-        self.assertEqual([(1.0, 1.0)], tracer.path)
-        pass
 
     def test_tracer_get_path(self):
         """
         the tracer will provide a method to return the path that has been traced as a list of points
         """
         tracer = Tracer()
-        tracer.trace(1, 90)
-        self.assertEqual([(0.0, 0.0), (0.0, 1.0)], tracer.path)
-        pass
-
-    def test_tracer_list_input(self):
-        """
-        the tracer will provide a way to pass in a set of move commands
-        """
-        tracer = Tracer()
-        tracer.trace([(1, 90), (1, 45)])
-        self.assertEqual([(0.0, 0.0), (0.0, 1.0), (-1.0, 1.0)], tracer.path)
-        pass
-
-    def test_tracer_is_closed(self):
-        """
-        the tracer will provide a static method that will determine if a path is closed
-        """
-        self.assertTrue(Tracer.is_closed([(1, 90), (1, 90), (1, 90), (1, 90)]))
-        self.assertFalse(Tracer.is_closed([(1, 90), (1, 90), (1, 90), (2, 90)]))
-        pass
+        tracer.trace(1.0, 180.0)
+        tracer.trace(1.0, 180.0)
+        self.assertListOfPointsAlmostEqual(tracer.path, [(0.0, 0.0), (1.0, 0.0), (0.0, 0.0)])
